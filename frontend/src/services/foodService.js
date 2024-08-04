5204416f98a7413a0259a3e05b83edb3ef8a6157
XuanHuy <22520553@gm.uit.edu.vn>
2024-08-04 16:10:39 +0700

    Solve conflict

--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -1,20 +1,3 @@
-<<<<<<< Updated upstream
-import { sample_foods, sample_tags } from "../data";
-
-export const getAll = async () => sample_foods;
-
-export const search = async searchTerm =>
-    sample_foods.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
-
-export const getAllTags = async () => sample_tags;
-
-export const getAllByTag = async tag => {
-    if (tag === "All") return getAll();
-    return sample_foods.filter(food => food.tags?.includes(tag));
-};
-
-export const getById = async id => sample_foods.find(food => food.id === id);
-=======
 import axios from "axios";
 
 export const getAll = async () => {
@@ -42,4 +25,4 @@ export const getById = async id => {
     const { data } = await axios.get(`/api/foods/${id}`);
     return data;
 }
->>>>>>> Stashed changes
+
