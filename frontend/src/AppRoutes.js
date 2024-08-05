5581819c8b612d89eb3705fea16303b1955c0671
wanghui <131352489+wanghui070404@users.noreply.github.com>
2024-08-05 16:22:14 +0700

    Update AppRoutes.js

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import CartPage from './pages/Cart/CartPage';
@@ -17,24 +16,4 @@ export default function AppRoutes() {
             <Route path="/cart" element={<CartPage />} />
         </Routes>
     );
-=======
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import CartPage from './pages/Cart/CartPage';
-import HomePage from './pages/Home/HomePage';
-import FoodPage from './pages/Food/FoodPage';
-
-
-
-export default function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search/:searchTerm" element={<HomePage />} />
-            <Route path="/tag/:tag" element={<HomePage />} />
-            <Route path="/food/:id" element={<FoodPage />} />
-            <Route path="/cart" element={<CartPage />} />
-        </Routes>
-    );
->>>>>>> Stashed changes
 }
